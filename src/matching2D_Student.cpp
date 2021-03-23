bd878d122dbba8ba0cc5941ce1c41b5139715805
Michael Walden <mwalden824@gmail.com>
2021-03-23 01:06:37 -0500

    Added k nearest neighbor

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -38,6 +38,17 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     { // k nearest neighbors (k=2)
 
         // ...
+        vector<vector<cv::DMatch>> knn_matches;
+        matcher->knnMatch(descSource, descRef, knn_matches, 2);
+
+        double minDescDistRatio = 0.8;
+        for (auto it = knn_matches.begin(); it != knn_matches.end(); ++it)
+        {
+            if ((*it)[0].distance < minDescDistRatio * (*it)[1].distance)
+            {
+                matches.push_back((*it)[0]);
+            }
+        }
     }
 }
 
